ce2555090ca185cde207efe867d2a962297a95c7
Byran <sunhaolin@hotoa.com>
2021-08-14 09:51:46 +0800

    新增 cfs.store类型 FS.Store.STEEDOSCLOUD 用于上传文件 到kong
    https://github.com/steedos/steedos-project-k8s/issues/35

---
+++ packages/cfs-steedos-cloud/checkNpm.js
@@ -0,0 +1,41 @@
+import {
+  checkNpmVersions
+} from 'meteor/tmeasday:check-npm-versions';
+
+const fs = require('fs');
+const path = require('path')
+
+if (Meteor.settings && Meteor.settings.cfs && Meteor.settings.cfs.steedosCloud) {
+  checkNpmVersions({
+    'aws-sdk': "^2.0.23"
+  }, 'steedos:cfs-steedos-cloud');
+
+  // // 修改s3-2006-03-01.min.json 将 SteedosApiKey添加进members用于请求时aws-sdk发送此header
+  // var base = process.cwd();
+  // console.log('process.cwd(): ', process.cwd());
+  // if (process.env.CREATOR_NODE_ENV == 'development') {
+  //   base = path.resolve('.').split('.meteor')[0];
+  // }
+  // console.log('base: ', base);
+  // var sdkPath = path.join(base, require.resolve('aws-sdk/package.json', {
+  //   paths: [base]
+  // }));
+  // console.log('sdkPath: ', sdkPath);
+  // var sdkVersionMinJsonPath = path.join(sdkPath, '../apis/s3-2006-03-01.min.json');
+  // console.log('sdkVersionMinJsonPath: ', sdkVersionMinJsonPath);
+  // var minJson = JSON.parse(fs.readFileSync(sdkVersionMinJsonPath));
+  // console.log(minJson);
+  // var operations = minJson.operations;
+  // for (const key in operations) {
+  //   if (Object.hasOwnProperty.call(operations, key)) {
+  //     const element = operations[key];
+  //     if (element.input) {
+  //       element.input.members.SteedosApiKey = { "location": "header", "locationName": "apikey" }
+  //     }
+  //   }
+  // }
+
+  // fs.writeFileSync(sdkVersionMinJsonPath, JSON.stringify(minJson)); 
+
+  AWS = require('aws-sdk');
+}
